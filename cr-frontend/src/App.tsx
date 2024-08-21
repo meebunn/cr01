c88d095053c3d0e712b6b59af9f6da6e75a85247
meebunn <thanapoommod@gmail.com>
2024-08-21 23:06:00 +0700

    use hook in frontend

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const App = () => {
+  const [message,setMessage] = useState("my message blah blah") ; 
 
-
-
-type AppState = {
-  message : string ; 
-} ; 
-
-class App extends React.Component<{}, AppState> {
-  state : AppState = {
-    message : "default message "
-  };
-
-  componentDidMount(){
-    fetch("http://localhost:3000/courses")
-      .then( res => res.json())
-      .then(obj => {
-        this.setState({message: obj.message});
-      });
-  }
-  
-  render() {
-    return (
-      <div>
-        {this.state.message}
-      </div>
-    );
-  }
+  useEffect(() => {
+          fetch("http://localhost:3000/courses")
+            .then( res => res.json())
+            .then(obj => {
+              setMessage(obj.message)
+            });
+  },[])
+  return (
+    <div className="App">
+      {message}
+    </div>
+  )
 }
+ 
+
+// type AppState = {
+//   message : string ; 
+// } ; 
+
+// class App extends React.Component<{}, AppState> {
+//   state : AppState = {
+//     message : "default message "
+//   };
+
+//   componentDidMount(){
+//     fetch("http://localhost:3000/courses")
+//       .then( res => res.json())
+//       .then(obj => {
+//         this.setState({message: obj.message});
+//       });
+//   }
+  
+//   render() {
+//     return (
+//       <div>
+//         {this.state.message}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
